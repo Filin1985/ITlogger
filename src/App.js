5e1c68f3569b89b22cac61c041b47a15d6cad619
Марат <ihsmen@iMac-Marat.local>
2020-10-20 19:11:11 +0300

    added techs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
-import AddLogModel from './components/logs/AddlogModal';
+import AddLogModel from './components/logs/AddLogModal';
+import EditLogModel from './components/logs/EditLogModal';
+import AddTechModel from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -18,6 +21,9 @@ const App = () => {
       <div className='container'>
         <AddBtn />
         <AddLogModel/>
+        <EditLogModel/>
+        <AddTechModel/>
+        <TechListModal/>
         <Logs/>
       </div>
       </Fragment>
